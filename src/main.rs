e4ed9046557538928c25d7d4324fc57bbe6ff5cc
Ryan Walker <ryanjwalker2001@gmail.com>
2024-07-09 11:23:43 +0100

    protocol

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
+mod client;
 mod protocol;
+mod server;
+mod settings;
+mod shared;
 
 fn main() {
     println!("Hello, world!");
--- src/protocol.rs
+++ src/protocol.rs
@@ -1,58 +1,163 @@
-use std::ops::{Add, Mul};
 
+use avian2d::prelude::*;
 use bevy::prelude::*;
-use client::ComponentSyncMode;
-use lightyear::prelude::*;
+use leafwing_input_manager::InputManagerBundle;
+use lightyear::{
+    prelude::*,
+    client::{
+        components::{ComponentSyncMode, LerpFn},
+        interpolation::LinearInterpolator
+    },
+    utils::avian2d::*,
+};
+use leafwing_input_manager::prelude::*;
 
-#[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
-pub enum Inputs {
-    Direction(Vec2),
-    Spawn,
-    Delete,
-    None,
+pub const BALL_SIZE: f32 = 15.0;
+pub const PLAYER_SIZE: f32 = 40.0;
+pub const REPLICATION_GROUP: ReplicationGroup = ReplicationGroup::new_id(1);
+
+#[derive(Bundle)]
+pub(crate) struct PlayerBundle {
+    id: PlayerId,
+    position: Position,
+    color: ColorComponent,
+    replicate: client::Replicate,
+    physics: PhysicsBundle,
+    inputs: InputManagerBundle<PlayerActions>,
+    pre_predicted: PrePredicted,
 }
 
-#[derive(Component, Serialize, Deserialize, Clone, Debug, PartialEq)]
-pub struct PlayerId(ClientId);
-
-#[derive(Component, Serialize, Deserialize, Clone, Debug, PartialEq, Deref, DerefMut)]
-pub struct PlayerPosition(Vec2);
-
-impl Add for PlayerPosition {
-    type Output = PlayerPosition;
-    #[inline]
-    fn add(self, rhs: PlayerPosition) -> PlayerPosition {
-        PlayerPosition(self.0.add(rhs.0))
+impl PlayerBundle {
+    pub(crate) fn new(id: ClientId, position: Vec2, input_map: InputMap<PlayerActions>) -> Self {
+        let color = color_from_id(id);
+        Self {
+            id: PlayerId(id),
+            position: Position(position),
+            color: ColorComponent(color),
+            replicate: client::Replicate {
+                group: REPLICATION_GROUP,
+                ..default()
+            },
+            physics: PhysicsBundle::player(),
+            inputs: InputManagerBundle::<PlayerActions> {
+                action_state: ActionState::default(),
+                input_map,
+            },
+            pre_predicted: PrePredicted::default(),
+        }
     }
 }
 
-impl Mul<f32> for &PlayerPosition {
-    type Output = PlayerPosition;
-    fn mul(self, rhs: f32) -> Self::Output {
-        PlayerPosition(self.0 * rhs)
+#[derive(Bundle)]
+pub(crate) struct BallBundle {
+    position: Position,
+    color: ColorComponent,
+    replicate: server::Replicate,
+    marker: BallMarker,
+    physics: PhysicsBundle
+}
+
+impl BallBundle {
+    pub(crate) fn new(position: Vec2, color: Color, predicted: bool) -> Self {
+        let mut sync_target = server::SyncTarget::default();
+        let mut group = ReplicationGroup::default();
+        if predicted {
+            sync_target.prediction = NetworkTarget::All;
+            group = REPLICATION_GROUP;
+        } else {
+            sync_target.interpolation = NetworkTarget::All;
+        }
+        let replicate = server::Replicate {
+            sync: sync_target,
+            group,
+            ..default()
+        };
+        Self {
+            position: Position(position),
+            color: ColorComponent(color),
+            replicate,
+            physics: PhysicsBundle::ball(),
+            marker: BallMarker,
+        }
     }
 }
 
+#[derive(Bundle)]
+pub(crate) struct PhysicsBundle {
+    pub(crate) collider: Collider,
+    pub(crate) collider_density: ColliderDensity,
+    pub(crate) rigid_body: RigidBody,
+}
+
+impl PhysicsBundle {
+    pub(crate) fn ball() -> Self {
+        Self {
+            collider: Collider::circle(BALL_SIZE),
+            collider_density: ColliderDensity(0.05),
+            rigid_body: RigidBody::Dynamic,
+        }
+    }
+
+    pub(crate) fn player() -> Self {
+        Self {
+            collider: Collider::rectangle(PLAYER_SIZE, PLAYER_SIZE),
+            collider_density: ColliderDensity(0.2),
+            rigid_body: RigidBody::Dynamic,
+        }
+    }
+}
+
+#[derive(Component, Serialize, Deserialize, Clone, Debug, PartialEq, Reflect)]
+pub struct PlayerId(pub ClientId);
+ 
 #[derive(Component, Serialize, Deserialize, Clone, Debug, PartialEq)]
-pub struct PlayerColor(pub(crate) Color);
+pub struct ColorComponent(pub(crate) Color);
+
+#[derive(Component, Serialize, Deserialize, Clone, Debug, PartialEq)]
+pub struct BallMarker;
+
+#[derive(Serialize, Deserialize, Debug, PartialEq, Eq, Clone, Copy, Hash, Reflect, Actionlike)]
+pub enum PlayerActions {
+    Up,
+    Down,
+    Left,
+    Right
+}
 
 pub struct ProtocolPlugin;
 
 impl Plugin for ProtocolPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins(InputPlugin::<Inputs>::default());
+        app.add_plugins(LeafwingInputPlugin::<PlayerActions>::default());
 
-        app.register_component::<PlayerId>(ChannelDirection::ServerToClient)
+        app.register_component::<PlayerId>(ChannelDirection::Bidirectional)
             .add_prediction(ComponentSyncMode::Once)
             .add_interpolation(ComponentSyncMode::Once);
 
-        app.register_component::<PlayerPosition>(ChannelDirection::ServerToClient)
+        app.register_component::<ColorComponent>(ChannelDirection::Bidirectional)
+            .add_prediction(ComponentSyncMode::Once)
+            .add_interpolation(ComponentSyncMode::Once);
+
+        app.register_component::<BallMarker>(ChannelDirection::Bidirectional)
+            .add_prediction(ComponentSyncMode::Once)
+            .add_interpolation(ComponentSyncMode::Once);
+
+        app.register_component::<Position>(ChannelDirection::Bidirectional)
             .add_prediction(ComponentSyncMode::Full)
             .add_interpolation(ComponentSyncMode::Full)
-            .add_linear_interpolation_fn();
+            .add_interpolation_fn(position::lerp)
+            .add_correction_fn(position::lerp);
 
-        app.register_component::<PlayerColor>(ChannelDirection::ServerToClient)
-            .add_prediction(ComponentSyncMode::Once)
-            .add_interpolation(ComponentSyncMode::Once);
+        app.register_component::<Rotation>(ChannelDirection::Bidirectional)
+            .add_prediction(ComponentSyncMode::Full)
+            .add_interpolation(ComponentSyncMode::Full)
+            .add_interpolation_fn(rotation::lerp)
+            .add_correction_fn(rotation::lerp);
+
+        app.register_component::<LinearVelocity>(ChannelDirection::Bidirectional)
+            .add_prediction(ComponentSyncMode::Full);
+
+        app.register_component::<AngularVelocity>(ChannelDirection::Bidirectional)
+            .add_prediction(ComponentSyncMode::Full);
     }
 }
